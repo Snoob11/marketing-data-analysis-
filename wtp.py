93a3af4c6b795bbccaff747f802562d5e60ad5fc
Snoob11 <renkwiz@gmail.com>
2022-11-17 15:02:32 +0100

    Update wtp.py

--- wtp.py
+++ wtp.py
@@ -1,14 +1,30 @@
+# analysis of wilingness to pay for plant based menu items
+
 import pandas as pd
-import hvplot.pandas 
+import hvplot.pandas
 import holoviews as hv
 
-prices = {'Too Cheap': [100,120,200,200,300,100,100,300,100,350,340,450,100,257,109,109,280,400,250,200],
-          'Cheap': [150,200,250,300,340,190,200,350,120,360,360,460,110,388,299,129,350,410,260,240],
-          'Expensive': [400,400,450,350,400,200,300,370,180,370,490,490,130,433,399,149,400,420,270,280],
-          'Too Expensive': [500,480,500,400,490,300,500,380,200,380,500,500,140,499,422,199,410,430,280,300],
-        }
+prices = pd.read_csv('C:/Users/socie/OneDrive/Documents/thesis 1 and 2/thesis 2/data/price1.csv',sep=';', engine='python')
+
+prices1 = prices.drop(['index','Frame'], axis =1, inplace=True)
+
+prices2 = pd.read_csv('C:/Users/socie/OneDrive/Documents/thesis 1 and 2/thesis 2/data/price2.csv',sep=';', engine='python')
+
+prices3 = prices2.drop(['index','Frame'], axis =1, inplace=True)
+
+prices4 = pd.read_csv('C:/Users/socie/OneDrive/Documents/thesis 1 and 2/thesis 2/data/price3.csv',sep=';', engine='python')
+
+prices5 = prices4.drop(['index','Frame'], axis =1, inplace=True)
+
 
 df = pd.DataFrame(prices)
+df1 = pd.DataFrame(prices2)
+df2 = pd.DataFrame(prices4)
+
+
+print(df)
+print(df1)
+print(df2)
 
 def price_sensitivity_meter(df, interpolate=False):
     # convert data from wide to long
@@ -67,8 +83,9 @@ def price_sensitivity_meter(df, interpolate=False):
     lower_line = hv.VLine(lower_bound).opts(color='grey', line_dash='dashed', line_width=0.4)
     upper_line = hv.VLine(upper_bound).opts(color='grey', line_dash='dashed', line_width=0.4)
 
-    
+   
     print(f'Optimal Price: ${optimal}')
-    print(f'Acceptable Price Range: ${lower_bound} to ${upper_bound}')
+    print(f'Acceptable Price Range: ${lower_bound} to ${upper_bound}') 
     
+
     return plot * lower_line * optimal_line * upper_line
